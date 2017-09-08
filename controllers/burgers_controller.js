952097f54b212f3bf76d92d85828dcbb1aad77c2
Squareof3 <haruharatakun@gmail.com>
2017-09-08 12:03:10 -0400

    Glory be to the Update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
      burgers: data
     };
